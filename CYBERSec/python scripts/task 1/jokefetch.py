fb9df0e610b309d2828105cda2f9f3013e8a63bb
root <root@localhost.localdomain>
2017-05-22 19:48:53 -0400

    cli

---
+++ CYBERSec/python scripts/task 1/jokefetch.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+from random import randint
+import time
+page_num = randint(1, 11)
+jokes={}
+url=("http://123hindijokes.com/very-funny-jokes/"+str(page_num))
+r = requests.get(url)
+data=BeautifulSoup(r.content,"lxml")
+joke=data.find_all("ul",{"class":"statusList"})
+for i in joke:
+    joke1 =i.find_all('li')
+    for joke2 in joke1:
+        joke3=joke2.get_text()
+        jokes[1]=joke3
+with open("hindijoke.json",'w') as joke3:
+    json.dump(jokes,joke3,ensure_ascii=False,indent=8)
+    time.sleep(15)
+    
---
+++ CYBERSec/python scripts/task 2/info.py
@@ -0,0 +1,33 @@
+import os
+import platform
+import json
+import sys
+import psutil
+import uuid
+from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
+import netifaces as ni
+import socket
+import requests
+REMOTE_SERVER = "www.google.com"
+def is_connected():
+    try:
+        host = socket.gethostbyname(REMOTE_SERVER)
+        s = socket.create_connection((host, 80), 2)
+        return True 
+    except:
+            pass
+            return False
+info={}
+info['system']={}
+info['system']['MAC'] = ':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])
+info['system']['processortype'] = platform.machine()
+info['system']['hostname'] = platform.node()
+info['system']['os'] = platform.system()
+info['system']['kernelversion'] = platform.release()
+if(is_connected()):    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    info['system']['private ip'] = s.getsockname()[0]
+    info['system']['public ip']=requests.get('http://ip.42.pl/raw').text
+with open('info.json', 'w') as outfile:
+    json.dump(info, outfile)
